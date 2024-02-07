2f5045d1e3dd0ed1f638a125298fe017898e9a98
cesar <cessar437@gmail.com>
2024-02-07 05:29:33 -0300

    header movido

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,22 +1,38 @@
+import React, { useState, useEffect } from 'react';
 import './header.css';
+import logo from "../../statics/imagenes/logo.jpg"
 
-export const header = () => {
+export const Header = () => {
+
+  const [prevScrollPos, setPrevScrollPos] = useState(window.scrollY);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      const visible = prevScrollPos > currentScrollPos;
+
+      setPrevScrollPos(currentScrollPos);
+      setVisible(visible);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [prevScrollPos]);
+
+
   return (
     
-    <div className="header">
-      <div className="lado">
-       <h1 className="texto"> C J<span className="tex3">D f</span> </h1>
-
-      </div>
-      <div className="otro-lado">
-
+    <div className={visible ? 'header' : 'header hide'}>
+       <img src={logo} alt="logo" className="logo" />
+      <div>
         <button type="button" className="iconos  btn btn-outline-secondary">Certificados</button>
         <button type="button" className="iconos  btn btn-outline-secondary">Proyectos</button>
         <button type="button" className="iconos  btn btn-outline-secondary">about</button>
         <button type="button" className="iconos  btn btn-outline-secondary">contacto</button>
-        
       </div>
- </div>
+     </div>
   )
 }
-export default header
+export default Header
